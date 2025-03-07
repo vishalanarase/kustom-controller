3669f76af60f20f3dd334f9153326741aee2b641
Vishal Anarase <iamvishalanarase@gmail.com>
2025-03-07 15:24:43 +0530

    Watch pods using clientset
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 
 	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -29,5 +31,18 @@ func main() {
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.WithError(err).Fatal("Failed to create clientset")
+	}
+
+	log.Info("Watch pods")
+	wiface, err := clientset.CoreV1().Pods("").Watch(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.WithError(err).Fatal("Failed to watch pods")
+	}
+
+	for {
+		select {
+		case msg := <-wiface.ResultChan():
+			log.Infof("Received event: %+v", msg)
+		}
 	}
 }
